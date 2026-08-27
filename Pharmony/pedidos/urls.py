c4383e742c0c03da9fb255418e3691a78ecc9365
Santiago-Beltran1 <pedrazasantiago837@gmail.com>
2026-08-27 14:45:13 -0500

    feat(pedidos): integración del módulo de pedidos

---
+++ Pharmony/pedidos/urls.py
@@ -0,0 +1,11 @@
+﻿from django.urls import path
+from . import views
+
+app_name = 'pedidos'
+
+urlpatterns = [
+    path('mis-pedidos/', views.mis_pedidos, name='mis_pedidos'),
+    path('seguimiento/', views.seguimiento_pedido, name='seguimiento_publico'),
+    path('seguimiento/<str:codigo>/', views.seguimiento_pedido, name='seguimiento_con_codigo'),
+    path('actualizar/<int:pedido_id>/', views.actualizar_estado_pedido, name='actualizar_estado'),
+]
